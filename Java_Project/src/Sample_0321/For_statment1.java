ad399af278c674800f79e65e89c43612f87f037d
yeop77 <ejrduq76@knu.ac.kr>
2016-03-28 20:46:38 +0900

    Hi

---
+++ Java_Project/src/Sample_0321/For_statment1.java
@@ -0,0 +1,18 @@
+package Sample_0321;
+
+public class For_statment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		int j = 10;
+		while (j >= 1) {
+			System.out.println(j * 10); // j*10
+			j--;
+
+		}
+	}
+
+}
---
+++ Java_Project/src/Sample_0321/for_2.java
@@ -0,0 +1,14 @@
+package Sample_0321;
+
+public class for_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 2; i++) {
+			for (int j = 1; j <= 3; j++) {
+				System.out.println(i + " " + j);
+			}
+		}
+	}
+
+}
