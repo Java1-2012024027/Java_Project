21857449972519e19fe05374d8f7d078b57226ca
yeop77 <ejrduq76@knu.ac.kr>
2016-04-25 20:41:10 +0900

    4.25

---
+++ Java_Project/src/Sample0425/MidTest.java
@@ -0,0 +1,32 @@
+package Sample0425;
+
+import java.util.Scanner;
+
+public class MidTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int menu = 0;
+		MidTest1 cc = new MidTest1();
+		Scanner input = new Scanner(System.in);
+		do {
+			System.out.println("1. I. S");
+			System.out.println("2. I. A");
+			System.out.println("3. Quit");
+			System.out.println("Input menu number : ");
+			menu = input.nextInt();
+
+			if (menu == 1)
+				cc.function1();
+			else if (menu == 2)
+				cc.function2();
+			else if (menu == 3)
+				System.out.println("You Missed");
+			else
+				System.out.println("Re-input menu number");
+
+		} while (menu != 3);
+
+	}
+
+}
