ba53a5866ae50351fb00df96fb5c95f3803cf72d
yeop77 <ejrduq76@knu.ac.kr>
2016-05-16 20:36:26 +0900

    ??

---
+++ Java_Project/src/Sample_0516/MyFrame.java
@@ -0,0 +1,28 @@
+package Sample_0516;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		setSize(300, 200);
+		setLocation(screenSize.width/2, screenSize.height / 2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("MyFrame");
+		Image img = kit.getImage("icon.gif");
+		setIconImage(img);
+		setLayout(new FlowLayout());
+		JButton button = new JButton("¹»ºÁ");
+		this.add(button);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		MyFrame f = new MyFrame();
+	}
+}
